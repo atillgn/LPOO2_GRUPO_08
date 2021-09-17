c43d0e1fac87413af297a6bdbffc95490ceb2d44
Marcia Velarde <marciavelarde1991@gmail.com>
2021-09-16 22:02:20 -0300

    Se agrega WinMesas

--- LPOO2_GRUPO_08/WinMesas.xaml.cs
+++ LPOO2_GRUPO_08/WinMesas.xaml.cs
@@ -18,9 +18,81 @@ namespace LPOO2_GRUPO_08
     /// </summary>
     public partial class WinMesas : Window
     {
+        private int mesa = 0;
+        private int cantMesas = 20;
+        private int cantColumnas = 0;
+        private int cantFilas = 0;
+
         public WinMesas()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            determinarOrden();
+            double anchoBoton = (mesas.Width - (10*cantColumnas)) / cantColumnas;
+            double altoBoton = (mesas.Height - (10*cantFilas)) / cantFilas;
+            for (int j = 0; j < cantMesas; j++)
+            {
+                mesa = mesa + 1;
+                Button temp = new Button();
+                temp.Height = altoBoton;
+                temp.Width = anchoBoton;
+                modificarBoton(temp);
+                mesas.Children.Add(temp);
+            }
+        }
+
+        private Button modificarBoton(Button temp)
+        {
+            temp.Margin = new Thickness(10, 0, 0, 10);
+            temp.Name = "button" + (mesa);
+            temp.Content = "Mesa " + (mesa);
+            if (temp.Content.ToString() == "Mesa 11" || temp.Content.ToString() == "Mesa 17")
+                temp.Background = Brushes.Red;
+            else
+                temp.Background = Brushes.Green;
+            temp.FontSize = 20;
+            temp.Foreground = Brushes.White;
+            temp.Click += new RoutedEventHandler(mesa_click);
+            return temp;
+        }
+
+        private void determinarOrden()
+        {
+            bool encontrado = false;
+            for (int m = 6; m > 3 && encontrado == false; m--)
+            {
+                if (cantMesas % m == 0)
+                {
+                    encontrado = true;
+                    cantFilas = cantMesas / m;
+                    cantColumnas = cantMesas / cantFilas;
+                }
+                else
+                {
+                    cantColumnas = 5;
+                    cantFilas = (cantMesas / cantColumnas) + 1;
+                }
+            }
+        }
+
+        private void mesa_click(Object sender, EventArgs e)
+        {
+            Button mesaSeleccionada = (Button)sender;
+            if (mesaSeleccionada.Background == Brushes.Green)
+            {
+                MessageBoxResult result = MessageBox.Show("Ocupar mesa?", "MESA LIBRE", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                    mesaSeleccionada.Background = Brushes.Red;
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Liberar mesa?", "MESA OCUPADA", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                    mesaSeleccionada.Background = Brushes.Green;
+            }
         }
 
         private void btnVolver_Click(object sender, RoutedEventArgs e)
@@ -30,10 +102,5 @@ namespace LPOO2_GRUPO_08
             this.Close();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            button11.Background = Brushes.Red;
-            button17.Background = Brushes.Red;
-        }
     }
 }
