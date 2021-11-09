0ac8a4c212b80da1e76845017ffabf7cd9796be9
Atilio Grafion <atiliollgn@gmail.com>
2021-11-09 21:29:42 -0200

    ABM Categoria

--- ClasesBase/TrabajarCategoria.cs
+++ ClasesBase/TrabajarCategoria.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data;
 using System.Data.SqlClient;
+using System.Collections.ObjectModel;
 
 namespace ClasesBase
 {
@@ -14,7 +15,7 @@ namespace ClasesBase
             return new SqlConnection(ClasesBase.Properties.Settings.Default.MyConnection);
         }
 
-        public DataTable TraerCategorias()
+        public static DataTable TraerCategorias()
         {
             SqlConnection cn = connection();
 
@@ -29,6 +30,75 @@ namespace ClasesBase
             da.Fill(dt);
 
             return dt;
+        }
+
+        public static ObservableCollection<Categoria> traerCategoriasObv()
+        {
+            DataTable dt = TraerCategorias();
+
+            var listaCategorias = new ObservableCollection<Categoria>();
+            foreach (DataRow r in dt.Rows)
+            {
+                listaCategorias.Add(transformarCategoria(r));
+            }
+
+            return (listaCategorias);
+        }
+
+        private static Categoria transformarCategoria(DataRow dt)
+        {
+            var oCategoria = new Categoria();
+            oCategoria.Cat_Id = (int)dt["Cat_Id"];
+            oCategoria.Cat_Descripcion = (string)dt["Cat_Descripcion"];
+            return oCategoria;
+        }
+
+        public static void agregarCategoria(Categoria oCat)
+        {
+            SqlConnection cn = connection();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "AltaCategoria";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cn;
+
+            cmd.Parameters.AddWithValue("@descripcion", oCat.Cat_Descripcion);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+
+        public static void borrarCategoria(int id)
+        {
+            SqlConnection cn = connection();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "BorrarCategoria";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cn;
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+
+        public static void editarCategoria(Categoria oCat)
+        {
+            SqlConnection cn = connection();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "ActualizarCategoria";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cn;
+
+            cmd.Parameters.AddWithValue("@id", oCat.Cat_Id);
+            cmd.Parameters.AddWithValue("@des", oCat.Cat_Descripcion);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
         }
     }
 }
--- LPOO2_GRUPO_08/WinABMCategoria.xaml.cs
+++ LPOO2_GRUPO_08/WinABMCategoria.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ClasesBase;
+using System.Collections.ObjectModel;
 
 namespace LPOO2_GRUPO_08
 {
@@ -18,6 +20,9 @@ namespace LPOO2_GRUPO_08
     /// </summary>
     public partial class WinABMCategoria : Window
     {
+        bool edit;
+        ObservableCollection<Categoria> obvCat;
+        Categoria catMod = new Categoria();
         public WinABMCategoria()
         {
             InitializeComponent();
@@ -60,6 +65,166 @@ namespace LPOO2_GRUPO_08
             {
                 this.DragMove();
             }
+        }
+
+        private Categoria cargarDatos()
+        {
+            Categoria oCategoria = new Categoria();
+            oCategoria.Cat_Descripcion = txtDescrip.Text;
+            return oCategoria;
+        }
+
+        private void cargarCampos()
+        {
+            txtDescrip.Text = catMod.Cat_Descripcion;
+        }
+
+        private string encadenarDatos(Categoria oCategoria)
+        {
+            string sCadenaDatos = "DATOS A GUARDAR: \n" +
+                                              "\n" +
+                                              "Descripcion: " + oCategoria.Cat_Descripcion + "\n";
+            return sCadenaDatos;
+        }
+
+        private void limpiarCampos()
+        {
+            txtDescrip.Clear();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ObjectDataProvider odp = (ObjectDataProvider)(this.Resources["listCat"]);
+            obvCat = odp.Data as ObservableCollection<Categoria>;
+            btnDisabled(btnModificar);
+            btnDisabled(btnEliminar);
+            edit = false;
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDescrip.Text != "")
+            {
+                if (edit)
+                {
+                    Categoria oCategoria = cargarDatos();
+                    MessageBoxResult result = MessageBox.Show(encadenarDatos(oCategoria) + "\n Guardar datos? \n", "", MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        try
+                        {
+                            oCategoria.Cat_Id = catMod.Cat_Id;
+                            TrabajarCategoria.editarCategoria(oCategoria);
+                            MessageBox.Show("DATOS MODIFICADOS CON EXITO!");
+                            limpiarCampos();
+                            btnDisabled(btnModificar);
+                            btnDisabled(btnEliminar);
+                            btnGuardar.Content = "Guardar";
+                        }
+                        catch (Exception a)
+                        {
+                            MessageBox.Show("La categoria ya se encuentra registrado", "ERROR CATEGORIA EXISTENTE");
+                            txtDescrip.Focus();
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        Categoria oCategoria = cargarDatos();
+                        MessageBoxResult result = MessageBox.Show(encadenarDatos(oCategoria) + "\n Guardar datos? \n", "", MessageBoxButton.YesNo);
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            TrabajarCategoria.agregarCategoria(oCategoria);
+                            MessageBox.Show("DATOS GUARDADOS CON EXITO!");
+                            limpiarCampos();
+                            obvCat.Add(oCategoria);
+                        }
+                    }
+                    catch (Exception a)
+                    {
+                        MessageBox.Show("La categoria ya se encuentra registrado", "ERROR CATEGORIA EXISTENTE");
+                        txtDescrip.Focus();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No pueden haber campos vacíos", "ERROR CAMPO VACÍO");
+            }
+        }
+
+        private void txtDescrip_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtDescrip_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void txtDescrip_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDisabled(Button b)
+        {
+            b.IsEnabled = false;
+            Style stl = Application.Current.FindResource("BtnDisLogin") as Style;
+            b.Style = stl;
+
+        }
+
+        private void btnEnable(Button b)
+        {
+            b.IsEnabled = true;
+            Style stl = Application.Current.FindResource("BtnLogin") as Style;
+            b.Style = stl;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lvCat.SelectedIndex != -1)
+            {
+                Categoria cat = (Categoria)lvCat.SelectedItem;
+                btnEnable(btnEliminar);
+                btnEnable(btnModificar);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            Categoria cat = (Categoria)lvCat.SelectedItem;
+            MessageBoxResult result = MessageBox.Show("Seguro que desea eliminar a \"" + cat.Cat_Descripcion + "\" ?", "ELIMINAR", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    TrabajarCategoria.borrarCategoria(cat.Cat_Id);
+                    MessageBox.Show("CATEGORIA ELIMINADA CON ÉXITO");
+                    btnDisabled(btnModificar);
+                    btnDisabled(btnEliminar);
+                    lvCat.SelectedIndex = -1;
+                    var FamDelete = obvCat.Single(i => i.Cat_Id == cat.Cat_Id);
+                    obvCat.Remove(FamDelete);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show("Esta familia esta vinculada a otros articulos", "ERROR ELIMINACION FAMILIA");
+                }
+
+            }
+        }
+
+        private void btnModificar_Click(object sender, RoutedEventArgs e)
+        {
+            edit = true;
+            catMod = (Categoria)lvCat.SelectedItem;
+            cargarCampos();
+            btnGuardar.Content = "Editar";
         }
     }
 }
