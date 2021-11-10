601142bc2779c844a3e1485c47e480f4e8045e9a
Atilio Grafion <atiliollgn@gmail.com>
2021-11-09 23:22:14 -0200

    ABM Unidades de Medida

--- ClasesBase/TrabajarUnidadMedida.cs
+++ ClasesBase/TrabajarUnidadMedida.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
 using System.Data;
+using System.Collections.ObjectModel;
 
 namespace ClasesBase
 {
@@ -14,7 +15,7 @@ namespace ClasesBase
             return new SqlConnection(ClasesBase.Properties.Settings.Default.MyConnection);
         }
 
-        public DataTable TraerUnidadMedidas()
+        public static DataTable TraerUnidadMedidas()
         {
             SqlConnection cn = connection();
 
@@ -29,6 +30,78 @@ namespace ClasesBase
             da.Fill(dt);
 
             return dt;
+        }
+
+        public static ObservableCollection<UnidadMedida> traerUnidadMedidaObv()
+        {
+            DataTable dt = TraerUnidadMedidas();
+
+            var listaUnidadMedida = new ObservableCollection<UnidadMedida>();
+            foreach (DataRow r in dt.Rows)
+            {
+                listaUnidadMedida.Add(transformarUnidadMedida(r));
+            }
+
+            return (listaUnidadMedida);
+        }
+
+        private static UnidadMedida transformarUnidadMedida(DataRow dt)
+        {
+            var oUnidad = new UnidadMedida();
+            oUnidad.Um_Id = (int)dt["Um_Id"];
+            oUnidad.Um_Abrev = (string)dt["Um_Abrev"];
+            oUnidad.Um_Descripcion = (string)dt["Um_Descripcion"];
+            return oUnidad;
+        }
+
+        public static void agregarUnidadMedida(UnidadMedida oUnidad)
+        {
+            SqlConnection cn = connection();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "AltaUnidadMedida";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cn;
+
+            cmd.Parameters.AddWithValue("@abrev", oUnidad.Um_Abrev);
+            cmd.Parameters.AddWithValue("@des", oUnidad.Um_Descripcion);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+
+        public static void borrarUnidadMedida(int id)
+        {
+            SqlConnection cn = connection();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "BorrarUnidadMedida";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cn;
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+
+        public static void editarUnidadMedida(UnidadMedida oUnidad)
+        {
+            SqlConnection cn = connection();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "ActualizarUnidadMedida";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cn;
+
+            cmd.Parameters.AddWithValue("@id", oUnidad.Um_Id);
+            cmd.Parameters.AddWithValue("@abrev", oUnidad.Um_Abrev);
+            cmd.Parameters.AddWithValue("@des", oUnidad.Um_Descripcion);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
         }
     }
 }
--- LPOO2_GRUPO_08/WinABMUnidadDeMedida.xaml.cs
+++ LPOO2_GRUPO_08/WinABMUnidadDeMedida.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ClasesBase;
+using System.Collections.ObjectModel;
 
 namespace LPOO2_GRUPO_08
 {
@@ -18,6 +20,9 @@ namespace LPOO2_GRUPO_08
     /// </summary>
     public partial class WinABMUnidadDeMedida : Window
     {
+        bool edit;
+        ObservableCollection<UnidadMedida> obvUni;
+        UnidadMedida uniMod = new UnidadMedida();
         public WinABMUnidadDeMedida()
         {
             InitializeComponent();
@@ -61,5 +66,195 @@ namespace LPOO2_GRUPO_08
                 this.DragMove();
             }
         }
+
+        private void btnDisabled(Button b)
+        {
+            b.IsEnabled = false;
+            Style stl = Application.Current.FindResource("BtnDisLogin") as Style;
+            b.Style = stl;
+
+        }
+
+        private void btnEnable(Button b)
+        {
+            b.IsEnabled = true;
+            Style stl = Application.Current.FindResource("BtnLogin") as Style;
+            b.Style = stl;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lvUnidad.SelectedIndex != -1)
+            {
+                UnidadMedida fam = (UnidadMedida)lvUnidad.SelectedItem;
+                /*
+                btnEliminar.IsEnabled = true;
+                btnModificar.IsEnabled = true;*/
+                btnEnable(btnEliminar);
+                btnEnable(btnModificar);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDescrip.Text != "")
+            {
+                if (edit)
+                {
+                    UnidadMedida oUnidad = cargarDatos();
+                    MessageBoxResult result = MessageBox.Show(encadenarDatos(oUnidad) + "\n Guardar datos? \n", "", MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        try
+                        {
+                            oUnidad.Um_Id = uniMod.Um_Id;
+                            TrabajarUnidadMedida.editarUnidadMedida(oUnidad);
+                            MessageBox.Show("DATOS MODIFICADOS CON EXITO!");
+                            limpiarCampos();
+                            btnDisabled(btnModificar);
+                            btnDisabled(btnEliminar);
+                            btnGuardar.Content = "Guardar";
+                        }
+                        catch (Exception a)
+                        {
+                            MessageBox.Show("La unidad ya se encuentra registrada", "ERROR UNIDAD EXISTENTE");
+                            txtDescrip.Focus();
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        UnidadMedida oUnidad = cargarDatos();
+                        MessageBoxResult result = MessageBox.Show(encadenarDatos(oUnidad) + "\n Guardar datos? \n", "", MessageBoxButton.YesNo);
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            TrabajarUnidadMedida.agregarUnidadMedida(oUnidad);
+                            MessageBox.Show("DATOS GUARDADOS CON EXITO!");
+                            limpiarCampos();
+                            obvUni.Add(oUnidad);
+                        }
+                    }
+                    catch (Exception a)
+                    {
+                        MessageBox.Show("La unidad ya se encuentra registrado", "ERROR UNIDAD EXISTENTE");
+                        txtDescrip.Focus();
+                        txtAbrev.Focus();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No pueden haber campos vacíos", "ERROR CAMPO VACÍO");
+            }
+        }
+
+        private void btnModificar_Click(object sender, RoutedEventArgs e)
+        {
+            edit = true;
+            uniMod = (UnidadMedida)lvUnidad.SelectedItem;
+            cargarCampos();
+            btnGuardar.Content = "Editar";
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            UnidadMedida uni = (UnidadMedida)lvUnidad.SelectedItem;
+            MessageBoxResult result = MessageBox.Show("Seguro que desea eliminar a \"" + uni.Um_Descripcion + "\" ?", "ELIMINAR", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    TrabajarUnidadMedida.borrarUnidadMedida(uni.Um_Id);
+                    MessageBox.Show("FAMILIA ELIMINADO CON ÉXITO");
+                    btnDisabled(btnModificar);
+                    btnDisabled(btnEliminar);
+                    lvUnidad.SelectedIndex = -1;
+                    var UniDelete = obvUni.Single(i => i.Um_Id == uni.Um_Id);
+                    obvUni.Remove(UniDelete);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show("Esta unidad esta vinculada a otros articulos", "ERROR ELIMINACION UNIDAD");
+                }
+
+            }
+        }
+
+        private UnidadMedida cargarDatos()
+        {
+            UnidadMedida oUnidad = new UnidadMedida();
+            oUnidad.Um_Descripcion = txtDescrip.Text;
+            oUnidad.Um_Abrev = txtAbrev.Text;
+            return oUnidad;
+        }
+
+        private void cargarCampos()
+        {
+            txtDescrip.Text = uniMod.Um_Descripcion;
+            txtAbrev.Text = uniMod.Um_Abrev;
+        }
+
+        private string encadenarDatos(UnidadMedida oUnidad)
+        {
+            string sCadenaDatos = "DATOS A GUARDAR: \n" +
+                                              "\n" +
+                                              "Descripcion: " + oUnidad.Um_Abrev + " " + oUnidad.Um_Descripcion + "\n";
+            return sCadenaDatos;
+        }
+
+        private void limpiarCampos()
+        {
+            txtDescrip.Clear();
+            txtAbrev.Clear();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ObjectDataProvider odp = (ObjectDataProvider)(this.Resources["listUnidad"]);
+            obvUni = odp.Data as ObservableCollection<UnidadMedida>;
+            btnDisabled(btnModificar);
+            btnDisabled(btnEliminar);
+            edit = false;
+        }
+
+        private void txtAbrev_LostFocus(object sender, RoutedEventArgs e)
+        {
+            lblDescrip.Foreground = Brushes.White;
+        }
+
+        private void txtDescrip_LostFocus(object sender, RoutedEventArgs e)
+        {
+            lblDescrip.Foreground = Brushes.White;
+        }
+
+        private void txtAbrev_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            btnDisabled(btnModificar);
+            btnDisabled(btnEliminar);
+            lvUnidad.SelectedIndex = -1;
+        }
+
+        private void txtDescrip_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            btnDisabled(btnModificar);
+            btnDisabled(btnEliminar);
+            lvUnidad.SelectedIndex = -1;
+        }
+
+        private void txtDescrip_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btnDisabled(btnModificar);
+            btnDisabled(btnEliminar);
+        }
+
+        private void txtAbrev_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btnDisabled(btnModificar);
+            btnDisabled(btnEliminar);
+        }
+
+
     }
 }
